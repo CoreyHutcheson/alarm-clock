2d7d9d95ae80efc23c405c8c8c01043375982ef3
Corey Hutcheson <hutcheson.coreyj@gmail.com>
2019-06-29 12:22:21 -0500

    Refactored Clock component

--- src/components/clock/Clock.jsx
+++ src/components/clock/Clock.jsx
@@ -3,19 +3,15 @@ import PropTypes from "prop-types";
 
 import styles from "./style.module.scss";
 
-export const Clock = ({ time }) => {
-  return (
-    <div>
-      <div className={styles.time}>
-        {time.toLocaleTimeString("en-US", {
-          hour: "2-digit",
-          minute: "2-digit",
-          second: "2-digit"
-        })}
-      </div>
-    </div>
-  );
-};
+export const Clock = ({ time }) => (
+  <div className={styles.time}>
+    {time.toLocaleTimeString("en-US", {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit"
+    })}
+  </div>
+);
 
 Clock.propTypes = {
   time: PropTypes.object.isRequired
