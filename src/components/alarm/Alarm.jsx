02e1bb9fcc825b206415812943f4e1c6a93dbc8c
Corey <hutcheson.coreyJ@gmail.com>
2019-06-29 00:30:27 -0500

    Initial implementation of Alarm component

--- src/components/alarm/Alarm.jsx
+++ src/components/alarm/Alarm.jsx
@@ -2,20 +2,43 @@ import React, { useState, useEffect } from "react";
 
 import { AlarmOptions } from "components/alarm-options";
 import { Clock } from "components/clock";
+import { getTime } from "utils/getTime";
 import styles from "./style.module.scss";
 
 export const Alarm = () => {
-  const [hours, setHours] = useState();
-  const [minutes, setMinutes] = useState();
+  const [alarmHours, setAlarmHours] = useState("off");
+  const [alarmMinutes, setAlarmMinutes] = useState(0);
+  const [currentTime, setCurrentTime] = useState(getTime());
 
-  const handleHourChange = e => {};
+  useEffect(() => {
+    setInterval(() => {
+      setCurrentTime(getTime());
+    }, 1000);
+  }, []);
 
-  const handleMinuteChange = e => {};
+  useEffect(() => {
+    const currentHours = currentTime.getHours();
+    const currentMinutes = currentTime.getMinutes();
+    // If time hasn't passed yet days = 0
+    // If time has already passed, days = 1;
+    // date.setTime(date.getTime() + 1 * 86400000);
+  }, [alarmHours, alarmMinutes]);
+
+  const handleHourChange = e => {
+    setAlarmHours(e.target.value);
+  };
+
+  const handleMinuteChange = e => {
+    setAlarmMinutes(e.target.value);
+  };
 
   return (
     <div>
-      <Clock />
-      <AlarmOptions />
+      <Clock time={currentTime} />
+      <AlarmOptions
+        handleHourChange={handleHourChange}
+        handleMinuteChange={handleMinuteChange}
+      />
     </div>
   );
 };
--- src/utils/getTime.js
+++ src/utils/getTime.js
@@ -1,6 +1,1 @@
-export const getTime = () =>
-  new Date().toLocaleTimeString("en-US", {
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit"
-  });
+export const getTime = () => new Date();
