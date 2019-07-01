df377ccdbd28a8922d02a900aaf4ecf300e652e8
Corey <hutcheson.coreyJ@gmail.com>
2019-07-01 18:54:22 -0500

    Renders ButtonContainer inside App | hooks up those buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import { ButtonContainer } from "components/app-buttons";
 import { Alarm } from "components/alarm";
 
-const App = () => (
-  <>
-    <Alarm />
-  </>
-);
+const App = () => {
+  const handleShowAllClick = () => {
+    alert("show all");
+  };
+
+  const handleNewAlarmClick = () => {
+    alert("new alarm");
+  };
+  return (
+    <>
+      <Alarm />
+      <ButtonContainer
+        handleShowAllClick={handleShowAllClick}
+        handleNewAlarmClick={handleNewAlarmClick}
+      />
+    </>
+  );
+};
 
 export default App;
